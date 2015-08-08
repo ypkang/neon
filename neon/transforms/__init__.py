35c6dbc258ab4c664dbfbfbc93b7d296298c6eeb
Yiping Kang <ypkang@umich.edu>
2015-08-08 00:18:10 -0400

    add htanh to init

--- neon/transforms/__init__.py
+++ neon/transforms/__init__.py
@@ -21,4 +21,6 @@ from neon.transforms.rectified import RectLin, RectLeaky  # noqa
 from neon.transforms.softmax import Softmax  # noqa
 from neon.transforms.sum_squared import SumSquaredDiffs  # noqa
 from neon.transforms.tanh import Tanh  # noqa
+from neon.transforms.htanh import HTanh
 from neon.transforms.xcov import XCovariance  # noqa
+
